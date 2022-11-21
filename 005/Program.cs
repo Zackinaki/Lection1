f003a3b8959579b5a6a1831ffe5385337eb58126
IrinchinovZh <exp.bur@gmail.com>
2022-11-21 18:26:00 +0800

    Add 005

---
+++ 005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Vvedite ima: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{   
+    Console.WriteLine("Ura! Masha");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
