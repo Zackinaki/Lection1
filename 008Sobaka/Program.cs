8b697fb7fb4c5b5a4ed8e89c87ab59d2ca17557a
IrinchinovZh <exp.bur@gmail.com>
2022-11-21 19:59:09 +0800

    Add 008sob

---
+++ 008Sobaka/Program.cs
@@ -0,0 +1,21 @@
+﻿int count = 0, time = 0;
+int distance = 100;
+int FistFriendSpeed = 1, SecondFriendSpeed = 2, DogSpeed = 5;
+int friend = 2;
+while(distance > 10)
+{
+    if(friend = 1)
+    {
+        time = distance/(FistFriendSpeed + DogSpeed);
+        friend = 2;
+    }
+    else
+    {
+        time = distance/(SecondFriendSpeed + DogSpeed);
+        friend = 1;
+    }
+    distance = distance - (FistFriendSpeed + SecondFriendSpeed)* time;
+    count++;
+}
+Console.WriteLine("Sobaka");
+Console.WriteLine(count);
